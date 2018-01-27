063e642964c5fff5f7591decd09e0ae87a7682ef
Carlos Isaac Martinez Batista <itscarlosisaac@gmail.com>
2018-01-27 18:57:38 -0400

    Add: Dinosaur data model.

--- src/db/Model.js
+++ src/db/Model.js
@@ -1,20 +1,18 @@
 const moongose = require('mongoose');
 const Schema = moongose.Schema;
 
-const DinoSchema = new Schema({
-    id: Number,
-    name: String,
-    region: String,
-    weight: Number,
-    size: Number,
-    period: String,
-    family: String,
-    description: String,
+const dinoModel = new Schema({
+    id: { type: Number },
+    name: { type: String },
+    region: { type: String },
+    weight: { type: Number },
+    size: { type: Number },
+    period: { type: String },
+    family: { type: String },
+    description: { type: String },
     createdAt: { type: Date,  default: Date.now },
     updatedAt: { type: Date,  default: Date.now }
 });
 
-const Dinosaur = moongose.model('Dinosaur', DinoSchema )
+module.exports = moongose.model('Dino', dinoModel )
 
-
-module.exports.Dino = Dinosaur;
